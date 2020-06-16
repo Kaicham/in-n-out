b80e91dd260bc8ddf96e1b4a179d90d03c9a16fb
Kaique Santana <ksantana@iouu.com.br>
2020-06-15 21:25:49 -0300

    Tela de login - html

--- public/index.php
+++ public/index.php
@@ -1,16 +1,5 @@
 <?php
 
 require_once(dirname(__FILE__, 2) . "/src/config/config.php");
-require_once(dirname(__FILE__, 2) . "/src/models/User.php");
+require_once(dirname(__FILE__, 2) . "/src/views/login.php");
 
-
-// $user = new User([ "name" => "lucas", "email" => "kaikham@hotmail.com" ]);
-// $user->name = "kaique";
-
-// echo User::getResultSetFromSelect() . "<br>";
-// print_r(  User::get(["name"=>"Chaves"]) );
-// echo User::getResultSetFromSelect(["id" => 1], "email") . "<br>";
-
-foreach(User::get() as $user) {
-    echo $user->name . "<br>";
-}
---
+++ src/views/login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/comum.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/icofont.min.css">
+    <link rel="stylesheet" href="assets/css/login.css">
+    <title>In N' Out</title>
+</head>
+<body>
+    <form action="#" method="post" class="form-login">
+        <div class="login-card card">
+            <div class="card-header">
+                <i class="icofont-travelling mr-2"></i>
+                <span class="font-weight-light">IN </span>
+                <span class="font-weight-bold mx-1">N' </span>
+                <span class="font-weight-light">Out</span>
+                <i class="icofont-runner-alt-1 ml-2"></i>
+            </div>
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="email">E-mail</label>
+                    <input type="email" name="email" id="email" class="form-control" placeholder="Informe o e-mail ..." autofocus>
+                </div>
+                <div class="form-group">
+                    <label for="password">Senha</label>
+                    <input type="password" name="password" id="password" class="form-control">
+                </div>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-lg btn-primary">Entrar</button>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
